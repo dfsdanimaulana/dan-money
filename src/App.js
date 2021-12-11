6db07fdccd180a945d112755798529573431f463
dfsdanimaulana <danimaulana9f@gmail.com>
2021-12-12 04:35:09 +0700

    user authentication flow react firebase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 
+// hooks
+import { useAuthContext } from './hooks/useAuthContext'
 // pages
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
@@ -9,22 +11,25 @@ import Signup from './pages/signup/Signup'
 import Navbar from './components/Navbar'
 
 function App() {
+  const { authIsReady, user } = useAuthContext()
   return (
     <div className="App">
-      <Router>
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/login">
-            <Login />
-          </Route>
-          <Route path="/signup">
-            <Signup />
-          </Route>
-        </Switch>
-      </Router>
+        { authIsReady && (
+          <Router>
+            <Navbar />
+            <Switch>
+              <Route exact path="/">
+                { user ? <Home /> : <Redirect to="/login" /> }
+              </Route>
+              <Route path="/login">
+                { !user ? <Login /> : <Redirect to="/" /> }
+              </Route>
+              <Route path="/signup">
+                { !user ? <Signup /> : <Redirect to="/" /> }
+              </Route>
+            </Switch>
+          </Router>
+        )}
     </div>
   )
 }
