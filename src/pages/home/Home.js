432d76fa5060eb92a92e569883181ca031af567b
dfsdanimaulana <danimaulana9f@gmail.com>
2021-12-12 19:28:19 +0700

    edit home

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,5 +2,12 @@
 import styles from './Home.module.css'
 
 export default function Home() {
-  return <div>Home</div>
+  return (
+      <div className={styles.container}>
+        <div className={styles.content}>
+        </div>
+        <div className={styles.sidebar}>
+        </div>
+      </div>
+  )
 }
